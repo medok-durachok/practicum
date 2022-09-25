f93675ace0b66d5b7b48d891942f3625b32670c1
Darya <s02210522@gse.cs,msu.ru>
2022-09-25 19:02:25 +0300

    task-2-matrix

--- task-2-matrix/matrix.c
+++ task-2-matrix/matrix.c
@@ -1,28 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-
-const int unsigned n = 3;
-const int unsigned m = 3;
-
-double * input_m(){
-    double * arr;
-    arr = malloc(n * m * sizeof(double));
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            scanf("%lf", &arr[i * n + j]);
-        }
-    }
-    return arr;
-}
-
-void output_m(double * m_arr){
-    for(int i = 0; i < n; i++){
-        for(int j = 0; j < m; j++){
-            printf("%.2lf\t", m_arr[i * n + j]);
-        }
-        printf("\n");
-    }
-}
+#include "matrix_lib.h"
 
 int main(){
     double * matrix = input_m();;
---
+++ task-2-matrix/matrix_lib.h
@@ -0,0 +1,7 @@
+#ifndef MATRIX_LIB_H_INCLUDED
+#define MATRIX_LIB_H_INCLUDED
+
+double * input_m();
+void output_m(double * m_arr);
+
+#endif
