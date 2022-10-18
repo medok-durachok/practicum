b5562352a561aef2c1bd98fbb24118956d19389f
Darya <s02210522@gse.cs,msu.ru>
2022-10-18 17:07:04 +0300

    fscanf

--- task4/7.33.c
+++ task4/7.33.c
@@ -4,51 +4,11 @@
 
 #define ADD_M 10
 
-char * get_S(FILE *f, int max_len) {
-    int n, k = 0;
-    char * s = malloc(max_len);
-
-    if(!s) return NULL;
-    while(fgets(s, max_len, f)) {
-        n = strlen(s);
-        if(s[n - 1] != '\n' && !feof(f)) {
-            k = k + max_len - 1;
-            if(!s) return NULL;
-        } else {
-            return s;
-        }
-    }
-    return NULL;
-}
-
-
-int get_L(FILE *f, int max_len) {
-    int n, k = 0;
-    char * s = malloc(max_len);
-
-    if(!s) return NULL;
-    while(fgets(s, max_len, f)) {
-        n = strlen(s);
-        if(s[n - 1] != '\n' && !feof(f)) {
-            k = k + max_len - 1;
-            if(!s) return -1;
-        } else {
-            if(s[n - 1] == '\n') {
-                s[n - 1] = '\0';
-                return n - 1;
-            } else {
-                return n;
-            }
-        }
-    }
-    return -1;
-}
-
 int main(int argc, char * argv[]) {
     FILE * f; int max_len, len = 0, count = 0, n = ADD_M;
-    f = fopen(argv[1], "rb+");
+    f = fopen(argv[1], "r+");
     max_len = atoi(argv[2]);
-    char *str = NULL, *s1 = NULL, *s2 = NULL;
+    char *str = malloc((max_len + 1) * sizeof(char)), *s1 = malloc((max_len + 1) * sizeof(char)), *s2 = malloc((max_len + 1) * sizeof(char));
 
     int *len_arr = calloc(n, sizeof(int)), *offset_arr = calloc(n, sizeof(int));
 
@@ -58,9 +18,11 @@ int main(int argc, char * argv[]) {
     }
 
     offset_arr[0] = 0;
-    while((len = get_L(f, max_len)) != -1) {
-        //printf("%d\n", len);
-        offset_arr[count + 1] = ftell(f);
+    while(fscanf(f, "%s", str) != -1) {
+        len = strlen(str);
+        str[len] = '\0';
+    //    printf("%d\n", len);
+        offset_arr[count + 1] = ftell(f) + 1;
         printf("%d\n", offset_arr[count]);
         len_arr[count] = len;
         count++;
@@ -80,14 +42,14 @@ int main(int argc, char * argv[]) {
                 len_arr[i] = len_arr[j];
                 len_arr[j] = tmp;
                 fseek(f, offset_arr[j], SEEK_SET);
-                s2 = get_S(f, max_len);
+                fscanf(f, "s", s2);
                 fseek(f, offset_arr[i], SEEK_SET);
-                s1 = get_S(f, max_len);
-                printf("%s %s\n", s1, s2);
-               /* fseek(f, offset_arr[j], SEEK_SET);
+                fscanf(f, "s", s1);
+              //  printf("%s %s\n", s1, s2);
+                fseek(f, offset_arr[j], SEEK_SET);
                 fwrite(s1, sizeof(char), 1, f);
                 fseek(f, offset_arr[i], SEEK_SET);
-                fwrite(s2, sizeof(char), 1, f);*/
+                fwrite(s2, sizeof(char), 1, f);
             }
         }
     }
