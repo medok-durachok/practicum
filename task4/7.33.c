87641b89187fe78551a5985e59667aa80e74d546
Darya <s02210522@gse.cs,msu.ru>
2022-10-18 19:59:48 +0300

    запись в доп. файл

--- task4/7.33.c
+++ task4/7.33.c
@@ -8,7 +8,7 @@ int main(int argc, char * argv[]) {
     FILE * f; int max_len, len = 0, count = 0, n = ADD_M;
     f = fopen(argv[1], "r+");
     max_len = atoi(argv[2]);
-    char *str = malloc((max_len + 1) * sizeof(char)), *s1 = malloc((max_len + 1) * sizeof(char)), *s2 = malloc((max_len + 1) * sizeof(char));
+    char *str = malloc((max_len + 1) * sizeof(char));
 
     int *len_arr = calloc(n, sizeof(int)), *offset_arr = calloc(n, sizeof(int));
 
@@ -21,9 +21,7 @@ int main(int argc, char * argv[]) {
     while(fscanf(f, "%s", str) != -1) {
         len = strlen(str);
         str[len] = '\0';
-    //    printf("%d\n", len);
         offset_arr[count + 1] = ftell(f) + 1;
-        printf("%d\n", offset_arr[count]);
         len_arr[count] = len;
         count++;
 
@@ -35,23 +33,31 @@ int main(int argc, char * argv[]) {
     }
     free(str);
 
+    FILE *tmp_f = fopen("tmp.txt", "w");
+    if (tmp_f == NULL) {
+        printf("Error opening file");
+        exit(1);
+    }
+
     for(int i = 0; i < count - 1; i++) {
-        for(int j = i; j < count; j++){
-            if(len_arr[i] > len_arr[j]) {
-                int tmp = len_arr[i];
-                len_arr[i] = len_arr[j];
-                len_arr[j] = tmp;
-                fseek(f, offset_arr[j], SEEK_SET);
-                fscanf(f, "s", s2);
-                fseek(f, offset_arr[i], SEEK_SET);
-                fscanf(f, "s", s1);
-              //  printf("%s %s\n", s1, s2);
-                fseek(f, offset_arr[j], SEEK_SET);
-                fwrite(s1, sizeof(char), 1, f);
-                fseek(f, offset_arr[i], SEEK_SET);
-                fwrite(s2, sizeof(char), 1, f);
+        for(int j = 0; j < count - i - 1; j++){
+            if(len_arr[j] > len_arr[j + 1]) {
+                int tmp = len_arr[j];
+                len_arr[j] = len_arr[j + 1];
+                len_arr[j + 1] = tmp;
+
+                tmp = offset_arr[j];
+                offset_arr[j] = offset_arr[j + 1];
+                offset_arr[j + 1] = tmp;
             }
         }
+    }
+
+    for(int i = 0; i < count; i++) {
+        fseek(f, offset_arr[i], SEEK_SET);
+        fscanf(f, "%s", str);
+        fwrite(str, sizeof(char*), 1, tmp_f);
+        fputc('\n', tmp_f);
     }
 
     fclose(f);
